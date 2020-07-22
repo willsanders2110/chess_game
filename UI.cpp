84b673546e7e32e5cb04a6d4b421cd975d73bfb1
willsanders2110 <willsanders2110@gmail.com>
2020-07-22 23:03:14 +0100

    Very rough board. Just for debugging really

--- UI.cpp
+++ UI.cpp
@@ -2,22 +2,33 @@
 
 void printBoard()
 {
-	cout << " A B C D E F G H \n";
+	cout << "      a       b       c       d       e       f       g       h\n";
 
 	for(int i=7; i >=0; i--)
 	{
 		if(i%2 == 0)
 		{
-			cout << "Black Line\n";
+			printLine(i, BLACK_SQUARE, WHITE_SQUARE);
 		}
 		else
 		{
-			cout << "White Line\n";
+			printLine(i, WHITE_SQUARE, BLACK_SQUARE);
 		}
 		
 	}
+	cout << "  -----------------------------------------------------------------\n";
+}
 
-void printLine(int Colour1, int Colour2)
+void printLine(int lineNumber, int colour1, int colour2)
 	{
-
+		cout << "  -----------------------------------------------------------------\n";
+		
+		cout << "  |       |       |       |       |       |       |       |       |\n";
+		cout << lineNumber+1;
+		cout << " |       |       |       |       |       |       |       |       |\n";
+		cout << "  |       |       |       |       |       |       |       |       |\n";
+		
+		// for(int i=0; i<7; i++)
+			
+		// cout << "\n";
 	}
